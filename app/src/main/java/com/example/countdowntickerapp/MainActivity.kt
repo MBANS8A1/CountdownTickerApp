cf8d8b1b92d732276067187735d20e61a9186737
MBANS8A1 <seanmbng@aol.com>
2025-06-25 20:30:35 +0100

    Initial commit of the application; changed the Scaffold in the MainActivity.kt file to a Surface.

---
+++ app/src/main/java/com/example/countdowntickerapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.countdowntickerapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.countdowntickerapp.ui.theme.CountdownTickerAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CountdownTickerAppTheme {
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting(name = "Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = "Hello $name!",
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CountdownTickerAppTheme {
+        Greeting("Android")
+    }
+}
