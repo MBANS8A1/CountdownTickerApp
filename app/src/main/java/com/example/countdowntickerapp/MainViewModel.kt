458b859cee432e7918f5825798cd34bfa1406a6a
MBANS8A1 <seanmbng@aol.com>
2025-06-26 16:21:33 +0100

    I have added the CountDownTimer object API in a timer variable and overrode the onTick() and onFinish() methods. Now I need to add the events to cause a change in the states.

--- app/src/main/java/com/example/countdowntickerapp/MainViewModel.kt
+++ app/src/main/java/com/example/countdowntickerapp/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.countdowntickerapp
 
+import android.os.CountDownTimer
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -12,12 +13,27 @@ class MainViewModel:ViewModel(){
         const val interval =  1000L
     }
     private val _currentTime : MutableStateFlow<Long> = MutableStateFlow(totalTime)
-    val currentTime:StateFlow<Long>
+    val currentTime: StateFlow<Long>
         get() = _currentTime
 
     private val _isTimerRunning: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val isTimerRunning:StateFlow<Boolean>
         get() = _isTimerRunning
+
+    private val timer: CountDownTimer = object : CountDownTimer(totalTime, interval){
+        override fun onTick(millisUntilFinished: Long) {
+            _currentTime.value =  millisUntilFinished
+        }
+
+        override fun onFinish() {
+            _currentTime.value = totalTime
+            _isTimerRunning.value = false
+        }
+
+    }
+
+
+
 
 
 }
