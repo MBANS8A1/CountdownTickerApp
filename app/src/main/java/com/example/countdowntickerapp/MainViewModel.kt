1d47e90dac5c319fccb7e5a0d6f7e712123d6855
MBANS8A1 <seanmbng@aol.com>
2025-06-26 11:42:52 +0100

    In the MainViewModel file, I have created a MutableStateFlow that is private and is an observed state (using the totalTime in the companion object). Also, the _currentTime is read-only and will be emitted to the collector (MainActivity) with the help of StateFlow.

--- app/src/main/java/com/example/countdowntickerapp/MainViewModel.kt
+++ app/src/main/java/com/example/countdowntickerapp/MainViewModel.kt
@@ -2,10 +2,12 @@ package com.example.countdowntickerapp
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class MainViewModel:ViewModel(){
-
-
+    private val _currentTime : MutableStateFlow<Long> = MutableStateFlow(totalTime)
+    val currentTime:StateFlow<Long>
+        get() = _currentTime
     companion object{
         //30 seconds
         const val totalTime = 30 * 1000L
