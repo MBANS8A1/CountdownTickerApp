699e38e59fa77a1deadaa413a034f689b2e63e4b
MBANS8A1 <seanmbng@aol.com>
2025-06-26 12:10:18 +0100

    I rearranged where the companion object was to get rid of the reference error for totalTime MutableStateFlow, and I also create an observable state to check if the countdown timer is running or not.

--- app/src/main/java/com/example/countdowntickerapp/MainViewModel.kt
+++ app/src/main/java/com/example/countdowntickerapp/MainViewModel.kt
@@ -5,14 +5,19 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 class MainViewModel:ViewModel(){
-    private val _currentTime : MutableStateFlow<Long> = MutableStateFlow(totalTime)
-    val currentTime:StateFlow<Long>
-        get() = _currentTime
     companion object{
         //30 seconds
         const val totalTime = 30 * 1000L
         //1 second
         const val interval =  1000L
     }
+    private val _currentTime : MutableStateFlow<Long> = MutableStateFlow(totalTime)
+    val currentTime:StateFlow<Long>
+        get() = _currentTime
+
+    private val _isTimerRunning: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val isTimerRunning:StateFlow<Boolean>
+        get() = _isTimerRunning
+
 
 }
