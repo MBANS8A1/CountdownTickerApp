4b1bfc627797dc8ef0684f9f56ffb2e1274c4da9
MBANS8A1 <seanmbng@aol.com>
2025-06-27 14:14:31 +0100

    In the Timer composable I added a Box layout with two Buttons and a Spacer in a Row layout. I am going to eventually use the parameters I passed into the composable.

--- app/src/main/java/com/example/countdowntickerapp/Timer.kt
+++ app/src/main/java/com/example/countdowntickerapp/Timer.kt
@@ -1,9 +1,31 @@
 package com.example.countdowntickerapp
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun Timer(modifier: Modifier = Modifier) {
-    
+fun Timer(currentTime: Long, isRunning:Boolean) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Row {
+            Button(onClick = {}) {
+                Text(text = "Start")
+            }
+            Spacer(modifier = Modifier.size(8.dp))
+            Button(onClick = {}) {
+                Text(text = "Start")
+            }
+        }
+    }
 }
