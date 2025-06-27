146180f7c95e9cbb8f63d67481fd1decca192b32
MBANS8A1 <seanmbng@aol.com>
2025-06-27 12:06:17 +0100

    I created a reference to the MainViewModel by using viewModels that will bridge between the ViewModel and the MainActivity (UI controller) and will allow me to use state in the MainActivity. I then used collectAsState() as part of the composable lifecycle, to allow the StateFlows to be converted to State so I can use the values.

--- app/src/main/java/com/example/countdowntickerapp/MainActivity.kt
+++ app/src/main/java/com/example/countdowntickerapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -11,11 +12,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.countdowntickerapp.ui.theme.CountdownTickerAppTheme
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,6 +32,11 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+    @Composable
+    fun CountDownTickerApp(){
+        val currentTime by viewModel.currentTime.collectAsState()
+        val isRunning by viewModel.isTimerRunning.collectAsState()
     }
 }
 
---
+++ app/src/main/java/com/example/countdowntickerapp/Timer.kt
@@ -0,0 +1,9 @@
+package com.example.countdowntickerapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Timer(modifier: Modifier = Modifier) {
+    
+}
