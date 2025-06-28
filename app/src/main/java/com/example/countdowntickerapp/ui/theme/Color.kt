c324b3d455c2c0ed51c17946f5d2e4dda745b043
MBANS8A1 <seanmbng@aol.com>
2025-06-28 14:57:03 +0100

    I added some custom colours, which I want to use with my progress timer(ticker) indicator when it is animated.

--- app/src/main/java/com/example/countdowntickerapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/countdowntickerapp/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val blue200 = Color(0xFF6FB1FC)
+val blue400 = Color(0xFF4363F7)
+val blue500 = Color(0xFF0052D4)
+val deepGold = Color(0xFFFFC850)
+val blueBG = Color(0xFF0C1B3A)
+val pink = Color(0xFFFFC3C3)
+val card = Color(0xFF162544)
