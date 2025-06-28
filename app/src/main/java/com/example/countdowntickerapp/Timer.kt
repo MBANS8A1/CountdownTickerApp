eaed5b7d2e520c05172f585132ff59a3d9435d11
MBANS8A1 <seanmbng@aol.com>
2025-06-28 12:08:16 +0100

    In the Timer composable, I used the experimental AnimatedContent composable to give the countdown ticker a spring based animation, when comparing the current time with the previous time using the targetState and transitionSpec arguments. I also formatted the text with different styling and font changes.

--- app/src/main/java/com/example/countdowntickerapp/Timer.kt
+++ app/src/main/java/com/example/countdowntickerapp/Timer.kt
@@ -1,15 +1,27 @@
 package com.example.countdowntickerapp
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.togetherWith
+import androidx.compose.animation.with
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -30,6 +42,29 @@ fun Timer(currentTime: Long,
                 Text(text = "Restart")
             }
         }
-        Text(text= getFormattedTime(currentTime))
+        AnimatedContent(targetState = currentTime,
+            transitionSpec = {
+                if(targetState > initialState){
+                   fadeIn() + slideInVertically(
+                       animationSpec = spring(),
+                       initialOffsetY = {fullHeight -> fullHeight }) togetherWith(
+                           fadeOut(animationSpec = spring())
+                       )
+
+                }else{
+                    fadeIn() + slideInVertically(
+                        animationSpec = spring(),
+                        initialOffsetY = {fullHeight -> fullHeight }) togetherWith(
+                            fadeOut(animationSpec = spring())
+                        )
+                }.using(SizeTransform(clip = false))
+            },
+            ) {time ->
+            Text(text= getFormattedTime(time),
+                 style =  MaterialTheme.typography.bodyLarge,
+                 fontWeight = FontWeight.Bold
+                )
+
+        }
     }
 }
